7a9ab441c92ebfcb51c46089c150c7ff7318bbdc
Lars Wobus <lars.wobus@web.de>
2019-01-15 23:36:45 +0100

    Simple progressbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import React from 'react';;
+import React from 'react';
+import Progress from 'react-progressbar';
 
 const App = () => {
     return (
       <div className="App">
         <header className="App-header">
+          <Progress completed={60}/>
           <p>
             The UI is coming soon.
           </p>
