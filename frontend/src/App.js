2f0289a6dbde97cb16cc98c71c69bf7fc6478aed
Lars Wobus <lars.wobus@web.de>
2019-01-15 23:22:18 +0100

    Default React configuration for upcoming projects

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';;
+
+const App = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            The UI is coming soon.
+          </p>
+        </header>
+      </div>
+    );
+}
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,50 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  context: __dirname,
+  entry: '.\\src\\index.js',
+  output: {
+    filename: 'main.js',
+    path: __dirname + '/dist',
+  },
+  module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 640000,
+          }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../'
+            }
+          },
+          "css-loader"
+        ]
+      }]
+    },
+};
