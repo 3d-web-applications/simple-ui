149d68822c307b2b40a1f09a87bca2d1ad77770b
Lars Wobus <lars.wobus@web.de>
2019-01-22 22:45:05 +0100

    Allows to change screens/views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,56 @@
 import './App.css';
+import './abilities.css';
+// import './screens/input-screen.css'
+import './colors.css';
+
 import React from 'react';
-import { RechargeableBattery } from './components/rechargeable-battery';
-import { CropFreeIcon } from './components/crop-free-icon';
-import { HelpCircleOutlineIcon } from './components/help-circle-outline-icon';
-import { CogwheelIcon } from './components/cogwheel-icon';
-import { PowerIcon } from './components/power-icon';
-import { RefillablePower } from './components/refillable-power';
+import { useState } from 'react';
 
-const temp1 = {
-  position: "absolute",
-  top: "2px",
-  left: "-4px",
-};
+import { WelcomeScreen } from './screens/welcome-screen';
+import { PlotScreen } from './screens/plot-screen';
+import { MainMenuScreen } from './screens/main-menu-screen';
+import { LoadingScreen } from './screens/loading-screen';
+import { IngameUi } from './components/ingame-ui';
 
-const temp2 = {
-  position: "absolute",
-  top: "2px",
-  left: "10px",
-};
-
-const temp3 = {
-  position: "absolute",
-  top: "2px",
-  left: "24px",
-};
-
-const temp4 = {
-  color: "#f00",
-  "-webkit-clip-path": "inset(10px 0px 0px 0px)",
-  "clip-path": "inset(10px 0px 0px 0px)",
-}
+import plot from './data/plot';
 
 const App = () => {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div style={temp1}><RechargeableBattery progress={100} /></div>
-          <div style={temp2}><RechargeableBattery progress={70} /></div>
-          <div style={temp3}><RechargeableBattery progress={0} /></div>
-          <div className="icon-container crop-free">
-            <CropFreeIcon />
-          </div>
-          <div className="icon-container help-circle-outline">
-            <HelpCircleOutlineIcon />
-          </div>
-          <div className="icon-container cogwheel">
-            <CogwheelIcon />
-          </div>
-          {/* <div className="icon-container power">
-            <PowerIcon />
-          </div>
-          <div className="icon-container power" >
-            <PowerIcon style={temp4}/>
-          </div> */}
-          <RefillablePower />
-          <p>
-            The UI is coming soon.
-          </p>
-        </header>
-      </div>
-    );
+  const [pageIndex, setPageIndex] = useState(1);
+
+  const enterPrologue = () => {
+    setPageIndex(2);
+  }
+
+  const enterMainMenu = () => {
+    setPageIndex(3);
+  }
+
+  const enterLoadingScreen = () => {
+    setPageIndex(4);
+  }
+
+  const enterNewGame = () => {
+    setPageIndex(5);
+  }
+
+  const SelectMenu = (value) => {
+    switch (value) {
+      case 1: return <WelcomeScreen onClick={enterPrologue} />;
+      case 2: return <PlotScreen plot={plot} onClick={enterMainMenu} />;
+      case 3: return <MainMenuScreen startNewGame={enterLoadingScreen} />;
+      case 4: return <LoadingScreen onClick={enterNewGame} />;
+      case 5: return <IngameUi />;
+      default: return null;
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {SelectMenu(pageIndex)}
+      </header>
+    </div>
+  );
 }
 
 export default App;
