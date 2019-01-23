d2c8c8626cad6389aa8f0be8d4f8ca96c3c2bef0
Lars Wobus <lars.wobus@web.de>
2019-01-23 21:35:42 +0100

    Overlay buttons can be clicked

--- frontend/src/screens/overlay-screen.js
+++ frontend/src/screens/overlay-screen.js
@@ -5,11 +5,36 @@ import React from 'react';
 export const OverlayScreen = () => {
   return <div className="overlay-screen">
     <div className="overlay-screen-button-group">
-      <div className="overlay-screen-button">Resume to Game</div>
-      <div className="overlay-screen-button">Save game</div>
-      <div className="overlay-screen-button">Load game</div>
-      <div className="overlay-screen-button">Options</div>
-      <div className="overlay-screen-button">Back to Main</div>
+      <div
+        className="overlay-screen-button"
+        onClick={() => {}}
+      >
+        Resume to Game
+      </div>
+      <div
+        className="overlay-screen-button"
+        onClick={() => {}}
+      >
+        Save game
+      </div>
+      <div
+        className="overlay-screen-button"
+        onClick={() => {}}
+      >
+        Load game
+      </div>
+      <div
+        className="overlay-screen-button"
+        onClick={() => {}}
+      >
+        Options
+      </div>
+      <div
+        className="overlay-screen-button"
+        onClick={() => {}}
+      >
+        Back to Main
+      </div>
     </div>
   </div>;
 };
