6cb7e2df9abb5ae95040d758d572aa315bab15e5
Lars Wobus <lars.wobus@web.de>
2019-01-23 22:02:56 +0100

    Prevent opening context menu in overlay screen

--- frontend/src/screens/overlay-screen.js
+++ frontend/src/screens/overlay-screen.js
@@ -3,35 +3,47 @@ import './overlay-screen.css';
 import React from 'react';
 
 export const OverlayScreen = ({closeOverlay, backToMain}) => {
-  return <div className="overlay-screen unselectable">
+  const hideContentMenu = (event) => {
+    event.preventDefault();
+  }
+
+  return <div
+      className="overlay-screen unselectable-text"
+      onContextMenu={hideContentMenu}
+    >
     <div className="overlay-screen-button-group">
       <div
         className="overlay-screen-button"
         onClick={closeOverlay}
+        onContextMenu={hideContentMenu}
       >
         Resume to Game
       </div>
       <div
         className="overlay-screen-button inactive"
         onClick={() => {}}
+        onContextMenu={hideContentMenu}
       >
         Save game
       </div>
       <div
         className="overlay-screen-button inactive"
         onClick={() => {}}
+        onContextMenu={hideContentMenu}
       >
         Load game
       </div>
       <div
         className="overlay-screen-button inactive"
         onClick={() => {}}
+        onContextMenu={hideContentMenu}
       >
         Options
       </div>
       <div
         className="overlay-screen-button"
         onClick={backToMain}
+        onContextMenu={hideContentMenu}
       >
         Back to Main
       </div>
