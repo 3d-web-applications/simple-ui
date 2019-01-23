862d387da000abfc22714746c9f91d2e07c8ecfb
Lars Wobus <lars.wobus@web.de>
2019-01-23 20:59:17 +0100

    Fix styling

--- frontend/src/util/calc-filling-level.js
+++ frontend/src/util/calc-filling-level.js
@@ -3,7 +3,7 @@ export const calcFillingLevel = (value) => {
   // min at 107, max at 16
   const progress = (91 * value) + 16;
   return {
-    '-webkit-clip-path': `inset(${progress}px 0px 0px 0px)`,
-    'clip-path': `inset(${progress}px 0px 0px 0px)`,
+    'WebkitClipPath': `inset(${progress}px 0px 0px 0px)`,
+    'clipPath': `inset(${progress}px 0px 0px 0px)`,
   };
 };
